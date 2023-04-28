962f39b1886b7291e2ca402cc376b5027ef725ab
Xinerki <simasadves@gmail.com>
2023-04-28 20:53:48 +0200

    goofy spacing

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ function PlayDropSound(pos)
 	-- local soundId = GetSoundId()
 	-- PlaySoundFromCoord(soundId, "Sonic_Drop_Rings", pos.x, pos.y, pos.z, "DLC_SONIC_SOUNDS", 0, 0, 0)
 	-- ReleaseSoundId(soundId)
-    PlaySoundFromCoord(-1, "Sonic_Drop_Rings", pos.x, pos.y, pos.z, "DLC_SONIC_SOUNDS", 0, 0, 0)
+	PlaySoundFromCoord(-1, "Sonic_Drop_Rings", pos.x, pos.y, pos.z, "DLC_SONIC_SOUNDS", 0, 0, 0)
 end
 
 function PlayPickSound()
@@ -17,38 +17,38 @@ RequestScriptAudioBank('dlc_sonic/sonic')
 RequestStreamedTextureDict("ring")
 
 function DrawSprite3D(textureDict, textureName, x, y, z, width, height, heading, red, green, blue, alpha)
-    x += math.sin(math.rad(-heading-90.0)) * (width*0.5)
-    y += math.cos(math.rad(-heading-90.0)) * (width*0.5)
-    --z = z -0.5
+	x += math.sin(math.rad(-heading-90.0)) * (width*0.5)
+	y += math.cos(math.rad(-heading-90.0)) * (width*0.5)
+	--z = z -0.5
 
-    local offX = math.sin(math.rad(-heading+90)) * width
-    local offY = math.cos(math.rad(-heading+90)) * width
+	local offX = math.sin(math.rad(-heading+90)) * width
+	local offY = math.cos(math.rad(-heading+90)) * width
 
-    DrawSpritePoly(
-        x+offX, y+offY, z, -- bottom right
-        x, y, z+height, -- top left
-        x, y, z, -- bottom left
-        red, green, blue, alpha,
-        textureDict,
-        textureName,
-        1.0, 0.0, 0.0,
-        0.0, -1.0, 0.0,
-        0.0, 0.0, 1.0)
-        
-    DrawSpritePoly(
-        x, y, z+height, -- top left
-        x+offX, y+offY, z, -- bottom right
-        x+offX, y+offY, z+height, -- top right
-        red, green, blue, alpha,
-        textureDict,
-        textureName,
-        -1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0)
+	DrawSpritePoly(
+		x+offX, y+offY, z, -- bottom right
+		x, y, z+height, -- top left
+		x, y, z, -- bottom left
+		red, green, blue, alpha,
+		textureDict,
+		textureName,
+		1.0, 0.0, 0.0,
+		0.0, -1.0, 0.0,
+		0.0, 0.0, 1.0)
+		
+	DrawSpritePoly(
+		x, y, z+height, -- top left
+		x+offX, y+offY, z, -- bottom right
+		x+offX, y+offY, z+height, -- top right
+		red, green, blue, alpha,
+		textureDict,
+		textureName,
+		-1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0)
 end
 
 function CreateRing(pos, vel)
-    CreateThread(function()
+	CreateThread(function()
 		Wait(math.random(0, 5) * 100)
 		
 		local spread = 0.5 + (math.random() * 0.5)
@@ -66,14 +66,14 @@ function CreateRing(pos, vel)
 			picked = false
 		}
 		
-        while GetGameTimer() < ring.life.start + ring.life.duration do Wait(0)
+		while GetGameTimer() < ring.life.start + ring.life.duration do Wait(0)
 		
 			-- PHYSICS
-            if not ring.picked then
-                ring.pos += ring.vel * GetFrameTime() * 2.0
-            end
-            ring.vel /= vector3(1.015, 1.015, 1.0)
-            ring.vel -= vector3(0.0, 0.0, 0.1)
+			if not ring.picked then
+				ring.pos += ring.vel * GetFrameTime() * 2.0
+			end
+			ring.vel /= vector3(1.015, 1.015, 1.0)
+			ring.vel -= vector3(0.0, 0.0, 0.1)
 			
 			local ringRadius = ring.size/2
 			local ringOrigin = ring.pos + vector3(0.0, 0.0, ringRadius)
@@ -81,39 +81,39 @@ function CreateRing(pos, vel)
 			local rayStart = ringOrigin
 			
 			local rayEnd = rayStart + vector3(-ringRadius, 0.0, 0.0)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitL = hit ~= 0
 			
 			local rayEnd = rayStart + vector3(ringRadius, 0.0, 0.0)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitR = hit ~= 0
 			
 			local rayEnd = rayStart + vector3(0.0, ringRadius, 0.0)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitF = hit ~= 0
 			
 			local rayEnd = rayStart + vector3(0.0, -ringRadius, 0.0)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitB = hit ~= 0
 			
 			local rayEnd = rayStart + vector3(0.0, 0.0, ringRadius)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitU = hit ~= 0
 			
 			local rayEnd = rayStart + vector3(0.0, 0.0, -ringRadius)
-            local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
-            local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(rayStart.x, rayStart.y, rayStart.z, rayEnd.x, rayEnd.y, rayEnd.z, -1, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
 			hitD = hit ~= 0
 			
 			local hit = hitU or hitD or hitL or hitR or hitF or hitB
 			
-            -- if hit ~= 0 and _end ~= vector3(0,0,0) then
-            if hit then
+			-- if hit ~= 0 and _end ~= vector3(0,0,0) then
+			if hit then
 				ring.vel *= vector3((hitL or hitR) and -1.0 or 1.0, (hitF or hitB) and -1.0 or 1.0, (hitU or hitD) and -0.75 or 1.0)
 							
 				-- if #(rayStart - _end) < #ring.size/2 then
@@ -122,27 +122,27 @@ function CreateRing(pos, vel)
 					-- ring.vel = getVelocityPoint(rayStart.x, rayStart.y, rayStart.z, _end.x, _end.y, _end.z, -#ring.vel)
 					-- DrawLine(rayStart, _end, 0, 255, 0, 255)
 				-- end
-            end
+			end
 			
 			-- RENDER
-            DrawLightWithRange(ring.pos.x, ring.pos.y, ring.pos.z, 255, 255, 102, 2.0, 0.5)
+			DrawLightWithRange(ring.pos.x, ring.pos.y, ring.pos.z, 255, 255, 102, 2.0, 0.5)
 
 			local tex = "ring"
-            local bit = math.floor(((GetGameTimer() - ring.life.start) % 400)/100)+1
-            if ring.picked then
-                tex = "star"
-            end
+			local bit = math.floor(((GetGameTimer() - ring.life.start) % 400)/100)+1
+			if ring.picked then
+				tex = "star"
+			end
 			
 			DrawSprite3D("ring", tex..bit, ring.pos.x, ring.pos.y, ring.pos.z, ring.size, ring.size, GetFinalRenderedCamRot().z, 255, 255, 255, 255)
 
 			-- PICKUP
-            if not IsEntityDead(PlayerPedId()) and not ring.picked and GetGameTimer() - ring.life.start > 500 and #(GetEntityCoords(PlayerPedId()) - ring.pos) < 1.0 then
-                PlayPickSound()
-                ring.picked = true
-                ring.life.start = GetGameTimer()
-                ring.life.duration = 500
-                ring.vel = vector3(0.0, 0.0, 0.0)
-            end
+			if not IsEntityDead(PlayerPedId()) and not ring.picked and GetGameTimer() - ring.life.start > 500 and #(GetEntityCoords(PlayerPedId()) - ring.pos) < 1.0 then
+				PlayPickSound()
+				ring.picked = true
+				ring.life.start = GetGameTimer()
+				ring.life.duration = 500
+				ring.vel = vector3(0.0, 0.0, 0.0)
+			end
 			
 			-- DEBUG
 			-- DrawLine(ringOrigin, ringOrigin + vector3(ringRadius, 0.0, 0.0), 255, 0, 0, 255)
@@ -152,8 +152,8 @@ function CreateRing(pos, vel)
 			-- DrawLine(ringOrigin, ringOrigin + vector3(0.0, 0.0, ringRadius), 0, 0, 255, 255)
 			-- DrawLine(ringOrigin, ringOrigin + vector3(0.0, 0.0, -ringRadius), 0, 0, 255, 255)
 			-- DrawMarker(28, ringOrigin, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ringRadius, ringRadius, ringRadius, 255, 255, 255, 50)
-        end
-    end)
+		end
+	end)
 end
 
 RegisterNetEvent("kgv:sonic:SpawnRings")
@@ -177,19 +177,19 @@ CreateThread(function()
 end)
 
 CreateThread(function()
-    while true do Wait(0)
-        local pool = GetGamePool("CPed")
-        for i,v in pairs(pool) do
-            if not IsPedAPlayer(v) and IsEntityDead(v) and not Entity(v).state.ringed then
+	while true do Wait(0)
+		local pool = GetGamePool("CPed")
+		for i,v in pairs(pool) do
+			if not IsPedAPlayer(v) and IsEntityDead(v) and not Entity(v).state.ringed then
 			Wait(100)
 				local pos = GetEntityCoords(v)
-                local vel = GetEntityVelocity(v)
+				local vel = GetEntityVelocity(v)
 				PlayDropSound(pos)
-                for i=0,GetPedMoney(v) do
-                    CreateRing(pos, vel)
-                end
-                Entity(v).state.ringed = true
-            end
-        end
-    end
+				for i=0,GetPedMoney(v) do
+					CreateRing(pos, vel)
+				end
+				Entity(v).state.ringed = true
+			end
+		end
+	end
 end)
